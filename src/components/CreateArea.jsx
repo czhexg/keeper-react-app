504f06dda7cfaa82ab90456ca701f75de0dbd469
Chan Zhe Xiang <czhexg1@gmail.com>
2022-12-23 22:48:12 +0800

    Update keeper app

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,8 +1,11 @@
 import { React, useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab, Zoom } from "@mui/material";
 
 function CreateArea(props) {
     const [noteTitle, setNoteTitle] = useState("");
     const [noteContent, setNoteContent] = useState("");
+    const [isFocused, setIsFocused] = useState(false);
 
     function updateTitle(event) {
         setNoteTitle(event.target.value);
@@ -12,30 +15,43 @@ function CreateArea(props) {
         setNoteContent(event.target.value);
     }
 
+    function updateFocus() {
+        setIsFocused(true);
+    }
+
+    function submitForm(event) {
+        if (noteTitle && noteContent) {
+            props.addNote(noteTitle, noteContent);
+            setNoteTitle("");
+            setNoteContent("");
+        }
+        event.preventDefault();
+    }
+
     return (
         <div>
-            <form
-                onSubmit={(event) => {
-                    props.addNote(noteTitle, noteContent);
-                    setNoteTitle("");
-                    setNoteContent("");
-                    event.preventDefault();
-                }}
-            >
-                <input
-                    name="title"
-                    placeholder="Title"
-                    value={noteTitle}
-                    onChange={updateTitle}
-                />
+            <form className="create-note" onSubmit={submitForm}>
+                {isFocused ? (
+                    <input
+                        name="title"
+                        placeholder="Title"
+                        value={noteTitle}
+                        onChange={updateTitle}
+                    />
+                ) : null}
                 <textarea
                     name="content"
                     placeholder="Take a note..."
                     rows="3"
                     value={noteContent}
                     onChange={updateContent}
+                    onFocus={updateFocus}
                 />
-                <button type="submit">Add</button>
+                <Zoom in={isFocused}>
+                    <Fab type="submit">
+                        <AddIcon />
+                    </Fab>
+                </Zoom>
             </form>
         </div>
     );
